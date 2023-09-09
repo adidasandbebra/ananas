f1ef0db7a6ae18b4674d57ac39dfc9f7e6e56af3
zoto_ff <zoto_ff@vk.com>
2023-09-09 20:25:18 +0300

    small windows fixes

--- webui-user.bat
+++ webui-user.bat
@@ -7,9 +7,9 @@ set callback_data={"server_id": 0, "type": ""}
 set callback_model=main
 
 set COMMANDLINE_ARGS=--listen --xformers --no-hashing --enable-insecure-extension-access --share --cloudflared --no-half-vae --disable-safe-unpickle --disable-console-progressbars --ui-settings-file settings.json --skip-torch-cuda-test --opt-channelslast --upcast-sampling --opt-sdp-attention
-set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.75,max_split_size_mb:512
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
 set PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 set CUDA_MODULE_LOADING=LAZY
 
-python -m accelerate launch --num_cpu_threads_per_process=6 launch.py
+"C:\Program Files\Python310\Scripts\accelerate.exe" launch --num_cpu_threads_per_process=6 launch.py
 pause
